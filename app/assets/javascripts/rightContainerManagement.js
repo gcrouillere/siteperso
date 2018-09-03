76e682d067deacb64b16876f4fd584db7b036e8d
guillaume crouillere <gcrouillere@hotmail.fr>
2018-09-03 09:37:36 +0200

    tablet vertical management

--- app/assets/javascripts/rightContainerManagement.js
+++ app/assets/javascripts/rightContainerManagement.js
@@ -3,9 +3,12 @@ if ($(window).innerWidth() / $(window).innerHeight() > 1.3) {
     $('.last-service').css({minHeight: "calc(100vh - 60px)"});
 }
 
-$(window).on( "orientationchange", function() {
+$(window).resize(function() {
   if ($(window).innerWidth() / $(window).innerHeight() > 1.3) {
     $('.services').css({minHeight: "calc(100vh - 100px)"});
     $('.last-service').css({minHeight: "calc(100vh - 60px)"});
+  } else {
+    $('.services').css({minHeight: "auto"});
+    $('.last-service').css({minHeight: "auto"});
   }
 });
